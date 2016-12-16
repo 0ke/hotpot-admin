1b886104338010aac749b117cd63a06856848365
kevin <kevinz@weghst.com>
2016-12-16 18:14:46 +0800

    修改 sidebar

--- src/pages/hp-aside-item.vue
+++ src/pages/hp-aside-item.vue
@@ -1,8 +1,28 @@
 <template>
-  <li v-for="m in menus" class="al-sidebar-list-item">
+  <ul class="al-sidebar-list">
+    <li v-for="m in menus" class="al-sidebar-list-item" :class="{'ba-sidebar-item-expanded': m.collapsed}">
+      <a class="al-sidebar-list-link" @click="toggle(m)">
+        <i v-if="m.icon" :class="m.icon"></i>
+        <span>{{m.title}}</span>
+        <i class="al-sidebar-menu-angle fa" :class="{'fa-angle-up': m.collapsed, 'fa-angle-down': !m.collapsed}"
+           v-if="m.subMenus"></i>
+      </a>
 
-  </li>
+      <hp-aside-item :menus="m.subMenus" v-if="m.subMenus"></hp-aside-item>
+    </li>
+  </ul>
 </template>
 <script>
-
+  export default {
+    name: 'hp-aside-item',
+    props: ['menus'],
+    methods: {
+      toggle(m) {
+        if (!m.subMenus) {
+          return
+        }
+        m.collapsed = !m.collapsed
+      }
+    }
+  }
 </script>
--- src/pages/hp-aside.vue
+++ src/pages/hp-aside.vue
@@ -1,29 +1,32 @@
 <template>
   <aside class="al-sidebar">
-    <ul class="al-sidebar-list">
-      <li v-for="m in menus" class="al-sidebar-list-item"
-          :class="{'ba-sidebar-item-expanded': m.collapsed}">
-        <a class="al-sidebar-list-link" @click="toggleMenu(m)">
-          <i v-if="m.icon" :class="m.icon"></i>
-          <span>{{m.title}}</span>
-          <i class="al-sidebar-menu-angle fa" :class="{'fa-angle-up': m.collapsed, 'fa-angle-down': !m.collapsed}"
-             v-if="m.subMenus"></i>
-        </a>
+    <!--<ul class="al-sidebar-list">-->
+    <!--<li v-for="m in menus" class="al-sidebar-list-item"-->
+    <!--:class="{'ba-sidebar-item-expanded': m.collapsed}">-->
+    <!--<a class="al-sidebar-list-link" @click="toggleMenu(m)">-->
+    <!--<i v-if="m.icon" :class="m.icon"></i>-->
+    <!--<span>{{m.title}}</span>-->
+    <!--<i class="al-sidebar-menu-angle fa" :class="{'fa-angle-up': m.collapsed, 'fa-angle-down': !m.collapsed}"-->
+    <!--v-if="m.subMenus"></i>-->
+    <!--</a>-->
 
-        <ul v-if="m.subMenus" class="al-sidebar-sublist">
-          <li v-for="sm in m.subMenus">
-            <a class="al-sidebar-list-link">
-              <i v-if="sm.icon" :class="sm.icon"></i>
-              <span>{{sm.title}}</span>
-            </a>
-          </li>
-        </ul>
-      </li>
-    </ul>
+    <!--<ul v-if="m.subMenus" class="al-sidebar-sublist">-->
+    <!--<li v-for="sm in m.subMenus">-->
+    <!--<a class="al-sidebar-list-link">-->
+    <!--<i v-if="sm.icon" :class="sm.icon"></i>-->
+    <!--<span>{{sm.title}}</span>-->
+    <!--</a>-->
+    <!--</li>-->
+    <!--</ul>-->
+    <!--</li>-->
+    <!--</ul>-->
+    <hp-aside-item :menus="menus"></hp-aside-item>
   </aside>
 </template>
 <script>
+  import hpAsideItem from './hp-aside-item.vue'
   export default {
+    components: {hpAsideItem},
     data() {
       return {
         menus: []
@@ -39,6 +42,7 @@
           var o = {
             title: r.meta.title,
             icon: r.meta.icon,
+            path: r.path,
             collapsed: false
           }
 
@@ -51,16 +55,6 @@
       }
       recMenu(routes, menus)
       this.menus = menus
-    },
-    methods: {
-      toggleMenu(m)
-      {
-        console.log(m)
-        if (!m.subMenus) {
-          return
-        }
-        m.collapsed = !m.collapsed
-      }
     }
   }
 </script>
